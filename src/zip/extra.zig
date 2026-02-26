9178c715c88f82f7ea016151cadd295badf0cf28
Kbgjtn <kambingjan31@gmail.com>
2026-02-26 15:46:58 +0000

    fix(example/zip): i think i found the correct pattern here for extra field parser

--- src/zip/extra.zig
+++ src/zip/extra.zig
@@ -10,7 +10,7 @@ pub const Context = union(enum) {
         local_file_header_relative_offset: ?u32,
         disk_number_start: ?u16,
     },
-    none,
+    none: void,
 };
 
 // TODO change to zip.HeaderId
@@ -21,27 +21,191 @@ pub const ParsedMetadata = union(enum) {
     info_zip_unix_new: InfoZipNewUnix,
 };
 
+fn Handler(comptime id: zip.HeaderId) type {
+    return switch (id) {
+        .zip64_extended_extra_field => Zip64Extended,
+        .extended_timestamp => ExtendedTimestamp,
+        .info_zip_unix_new => InfoZipNewUnix,
+        else => UnimplementedParser,
+    };
+}
+
+const UnimplementedParser = struct {
+    pub fn parse(data: []const u8, ctx: Context) !ParsedMetadata {
+        _ = data;
+        _ = ctx;
+        return error.Unimplemented;
+    }
+};
+
+pub const Zip64Extended = struct {
+    uncompressed_size: u64 = 0,
+    compressed_size: u64 = 0,
+    local_file_header_relative_offset: u64 = 0,
+    disk_number_start: u32 = 0,
+
+    fn parse(
+        data: []const u8,
+        ctx: Context,
+    ) !ParsedMetadata {
+        var output: Zip64Extended = .{};
+        var reader = std.Io.Reader.fixed(data);
+
+        if (ctx.zip64_extended_extra_field.uncompressed_size) |v| {
+            output.uncompressed_size =
+                if (v == 0xFFFFFFFF) try reader.takeInt(u64, .little) else @intCast(v);
+        }
+
+        if (ctx.zip64_extended_extra_field.compressed_size) |v| {
+            output.compressed_size =
+                if (v == 0xFFFFFFFF) try reader.takeInt(u64, .little) else @intCast(v);
+        }
+
+        if (ctx.zip64_extended_extra_field.local_file_header_relative_offset) |v| {
+            output.local_file_header_relative_offset =
+                if (v == 0xFFFFFFFF)
+                    try reader.takeInt(u64, .little)
+                else
+                    @intCast(v);
+        }
+
+        if (ctx.zip64_extended_extra_field.disk_number_start) |v| {
+            output.disk_number_start =
+                if (v == 0xFFFF)
+                    try reader.takeInt(u32, .little)
+                else
+                    @intCast(v);
+        }
+
+        if (reader.end != data.len) return error.Malformed;
+        return .{ .zip64_extended_extra_field = output };
+    }
+};
+
+// TODO add test cases
+test "parse ZIP64 extended information" {
+    const ef: Field = .{ .data = &[_]u8{ 6, 0, 0, 0, 0, 0, 0, 0 }, .id = 0x1 };
+    const parsed = try Zip64Extended.parse(ef.data, .{
+        .zip64_extended_extra_field = .{
+            .compressed_size = 6,
+            .uncompressed_size = 4294967295,
+            .local_file_header_relative_offset = 0,
+            .disk_number_start = 0,
+        },
+    });
+
+    try std.testing.expect(parsed.zip64_extended_extra_field.disk_number_start == 0);
+    try std.testing.expect(@TypeOf(parsed.zip64_extended_extra_field.disk_number_start) == u32);
+    try std.testing.expect(parsed.zip64_extended_extra_field.local_file_header_relative_offset == 0);
+    try std.testing.expect(@TypeOf(parsed.zip64_extended_extra_field.local_file_header_relative_offset) == u64);
+    try std.testing.expect(parsed.zip64_extended_extra_field.uncompressed_size == 6);
+    try std.testing.expect(@TypeOf(parsed.zip64_extended_extra_field.uncompressed_size) == u64);
+    try std.testing.expect(parsed.zip64_extended_extra_field.compressed_size == 6);
+    try std.testing.expect(@TypeOf(parsed.zip64_extended_extra_field.compressed_size) == u64);
+}
+
+pub const ExtendedTimestamp = struct {
+    mod_time_unix: ?u32 = null,
+    access_time_unix: ?u32 = null,
+    creation_time_unix: ?u32 = null,
+
+    // TODO (dapa) unit test
+    fn parse(data: []const u8, _: Context) !ParsedMetadata {
+        var output: ExtendedTimestamp = .{};
+        if (data.len < 1) return error.Empty;
+
+        const flags = data[0];
+        var offset: usize = 1;
+
+        if ((flags & 0x01) != 0 and offset + 4 <= data.len) {
+            output.mod_time_unix = std.mem.readInt(u32, data[offset..][0..4], .little);
+            offset += 4;
+        }
+
+        if ((flags & 0x02) != 0 and offset + 4 <= data.len) {
+            output.access_time_unix = std.mem.readInt(u32, data[offset..][0..4], .little);
+            offset += 4;
+        }
+
+        if ((flags & 0x04) != 0 and offset + 4 <= data.len) {
+            output.creation_time_unix = std.mem.readInt(u32, data[offset..][0..4], .little);
+        }
+
+        if (offset != data.len) {
+            return error.Malformed;
+        }
+
+        return .{ .extended_timestamp = output };
+    }
+};
+
+/// Currently stores Unix UIDs/GIDs up to 32 bits.
+///         Value         Size        Description
+///         -----         ----        -----------
+/// (UnixN) 0x7875        Short       tag for this extra block type ("ux")
+///         TSize         Short       total data size for this block
+///         Version       1 byte      version of this extra field, currently 1
+///         UIDSize       1 byte      Size of UID field
+///         UID           Variable    UID for this entry
+///         GIDSize       1 byte      Size of GID field
+///         GID           Variable    GID for this entry
+const InfoZipNewUnix = struct {
+    version: ?u8,
+    uid: ?u32,
+    gid: ?u32,
+
+    // TODO (dapa) unit test
+    fn parse(data: []const u8, _: Context) !ParsedMetadata {
+        var output: InfoZipNewUnix = undefined;
+        if (data.len < 1) return error.Empty;
+
+        var reader: std.Io.Reader = .fixed(data);
+
+        // version of this extra field, currently 1
+        output.version = try reader.takeByte();
+        if (output.version != 1) {
+            return error.BadVersion;
+        }
+
+        const uid_size = try reader.takeByte();
+        if (uid_size > 0 and reader.seek + uid_size <= data.len) {
+            output.uid = try reader.takeInt(u32, .little);
+        }
+
+        const gid_size = try reader.takeByte();
+        if (gid_size > 0 and reader.seek + gid_size <= data.len) {
+            output.gid = try reader.takeInt(u32, .little);
+        }
+
+        if (reader.end != data.len) {
+            return error.Malformed;
+        }
+
+        return .{ .info_zip_unix_new = output };
+    }
+};
+
 pub const Field = struct {
     id: u16,
     data: []const u8,
 
     pub fn asHeaderID(self: Field) ?zip.HeaderId {
-        return std.enums.fromInt(zip.HeaderId, self.id);
+        return std.enums.fromInt(zip.HeaderId, self.id) orelse null;
     }
 
     pub fn parse(self: Field, ctx: Context) !ParsedMetadata {
-        const id = self.asHeaderID() orelse return error.BadHeaderId;
+        // NOTE (dapa)
+        // converting runtime ID into enum HeaderId type, handling unknown
+        // correct me if Im wrong, because value known at runtime cannot be
+        // used at comptime.
+        // we do not need asHeaderID to comptime, anyway!
+        const id = self.asHeaderID() orelse return error.Unknown;
+
+        // then, we dispatch to the correct parser using compile-time mapping
         return switch (id) {
-            .zip64_extended_extra_field => .{
-                .zip64_extended_extra_field = try parseZip64Extended(self.data, ctx),
+            inline else => |comptime_id| {
+                return try Handler(comptime_id).parse(self.data, ctx);
             },
-            .extended_timestamp => .{
-                .extended_timestamp = try parseExtendedTimestamp(self.data, ctx),
-            },
-            .info_zip_unix_new => .{
-                .info_zip_unix_new = try parseInfoZipUnixNew(self.data, ctx),
-            },
-            else => error.BadHeaderId,
         };
     }
 };
@@ -82,153 +246,6 @@ test "extra field parse" {
 
         try std.testing.expectEqualDeep(output, expected);
     }
-}
-
-pub const Zip64Extended = struct {
-    uncompressed_size: u64 = 0,
-    compressed_size: u64 = 0,
-    local_file_header_relative_offset: u64 = 0,
-    disk_number_start: u32 = 0,
-};
-
-fn parseZip64Extended(
-    data: []const u8,
-    ctx: Context,
-) !Zip64Extended {
-    var result: Zip64Extended = .{};
-    var reader = std.Io.Reader.fixed(data);
-
-    if (ctx.zip64_extended_extra_field.uncompressed_size) |v| {
-        result.uncompressed_size =
-            if (v == 0xFFFFFFFF) try reader.takeInt(u64, .little) else @intCast(v);
-    }
-
-    if (ctx.zip64_extended_extra_field.compressed_size) |v| {
-        result.compressed_size =
-            if (v == 0xFFFFFFFF) try reader.takeInt(u64, .little) else @intCast(v);
-    }
-
-    if (ctx.zip64_extended_extra_field.local_file_header_relative_offset) |v| {
-        result.local_file_header_relative_offset =
-            if (v == 0xFFFFFFFF)
-                try reader.takeInt(u64, .little)
-            else
-                @intCast(v);
-    }
-
-    if (ctx.zip64_extended_extra_field.disk_number_start) |v| {
-        result.disk_number_start =
-            if (v == 0xFFFF)
-                try reader.takeInt(u32, .little)
-            else
-                @intCast(v);
-    }
-
-    if (reader.end != data.len) return error.Malformed;
-    return result;
-}
-
-// TODO add test cases
-test "parse ZIP64 extended information" {
-    const ef: Field = .{ .data = &[_]u8{ 6, 0, 0, 0, 0, 0, 0, 0 }, .id = 0x1 };
-    const parsed = try parseZip64Extended(ef.data, .{
-        .zip64_extended_extra_field = .{
-            .compressed_size = 6,
-            .uncompressed_size = 4294967295,
-            .local_file_header_relative_offset = 0,
-            .disk_number_start = 0,
-        },
-    });
-
-    try std.testing.expect(parsed.disk_number_start == 0);
-    try std.testing.expect(@TypeOf(parsed.disk_number_start) == u32);
-    try std.testing.expect(parsed.local_file_header_relative_offset == 0);
-    try std.testing.expect(@TypeOf(parsed.local_file_header_relative_offset) == u64);
-    try std.testing.expect(parsed.uncompressed_size == 6);
-    try std.testing.expect(@TypeOf(parsed.uncompressed_size) == u64);
-    try std.testing.expect(parsed.compressed_size == 6);
-    try std.testing.expect(@TypeOf(parsed.compressed_size) == u64);
-}
-
-pub const ExtendedTimestamp = struct {
-    mod_time_unix: ?u32 = null,
-    access_time_unix: ?u32 = null,
-    creation_time_unix: ?u32 = null,
-};
-
-// TODO (dapa) unit test
-fn parseExtendedTimestamp(data: []const u8, _: Context) !ExtendedTimestamp {
-    var result: ExtendedTimestamp = .{};
-    if (data.len < 1) return error.Empty;
-
-    const flags = data[0];
-    var offset: usize = 1;
-
-    if ((flags & 0x01) != 0 and offset + 4 <= data.len) {
-        result.mod_time_unix = std.mem.readInt(u32, data[offset..][0..4], .little);
-        offset += 4;
-    }
-
-    if ((flags & 0x02) != 0 and offset + 4 <= data.len) {
-        result.access_time_unix = std.mem.readInt(u32, data[offset..][0..4], .little);
-        offset += 4;
-    }
-
-    if ((flags & 0x04) != 0 and offset + 4 <= data.len) {
-        result.creation_time_unix = std.mem.readInt(u32, data[offset..][0..4], .little);
-    }
-
-    if (offset != data.len) {
-        return error.Malformed;
-    }
-
-    return result;
-}
-
-/// Currently stores Unix UIDs/GIDs up to 32 bits.
-///         Value         Size        Description
-///         -----         ----        -----------
-/// (UnixN) 0x7875        Short       tag for this extra block type ("ux")
-///         TSize         Short       total data size for this block
-///         Version       1 byte      version of this extra field, currently 1
-///         UIDSize       1 byte      Size of UID field
-///         UID           Variable    UID for this entry
-///         GIDSize       1 byte      Size of GID field
-///         GID           Variable    GID for this entry
-const InfoZipNewUnix = struct {
-    version: ?u8,
-    uid: ?u32,
-    gid: ?u32,
-};
-
-// TODO (dapa) unit test
-fn parseInfoZipUnixNew(data: []const u8, _: Context) !InfoZipNewUnix {
-    var output: InfoZipNewUnix = undefined;
-    if (data.len < 1) return error.Empty;
-
-    var reader: std.Io.Reader = .fixed(data);
-
-    // version of this extra field, currently 1
-    output.version = try reader.takeByte();
-    if (output.version != 1) {
-        return error.BadVersion;
-    }
-
-    const uid_size = try reader.takeByte();
-    if (uid_size > 0 and reader.seek + uid_size <= data.len) {
-        output.uid = try reader.takeInt(u32, .little);
-    }
-
-    const gid_size = try reader.takeByte();
-    if (gid_size > 0 and reader.seek + gid_size <= data.len) {
-        output.gid = try reader.takeInt(u32, .little);
-    }
-
-    if (reader.end != data.len) {
-        return error.Malformed;
-    }
-
-    return output;
 }
 
 // TODO (dapa) unit test
